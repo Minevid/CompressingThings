6f0e43b6f54416f84d200d8b7727ed2ae198a401
Reynout De Wolf <reynout.dewolf.y0468@student.hogent.be>
2017-03-13 12:29:45 +0100

    New Mod Startup + first block

--- src/main/java/com/minevid/compressingthings/blocks/ModBlocks.java
+++ src/main/java/com/minevid/compressingthings/blocks/ModBlocks.java
@@ -1,31 +1,41 @@
 package com.minevid.compressingthings.blocks;
 
-import com.minevid.compressingthings.blocks.Dirt.BlockCompressedDirt;
-import com.minevid.compressingthings.blocks.Dirt.BlockDoubleCompressedDirt;
-import com.minevid.compressingthings.blocks.Dirt.BlockTripleCompressedDirt;
+import com.minevid.compressingthings.blocks.Dirt.*;
 
 /**
  * Created by Reynout on 9/03/2017.
  */
-public class ModBlocks
-{
+public class ModBlocks {
     public static BlockCompressedDirt compressedDirt;
     public static BlockDoubleCompressedDirt doubleCompressedDirt;
     public static BlockTripleCompressedDirt tripleCompressedDirt;
+    public static BlockQuadrupleCompressedDirt quadrupleCompressedDirt;
+    public static BlockQuintupleCompressedDirt quintupleCompressedDirt;
+    public static BlockSextupleCompressedDirt sextupleCompressedDirt;
+    public static BlockSeptupleCompressedDirt septupleCompressedDirt;
+    public static BlockOctupleCompressedDirt octupleCompressedDirt;
 
 
-    public static void init()
-    {
+    public static void init() {
         compressedDirt = new BlockCompressedDirt();
         doubleCompressedDirt = new BlockDoubleCompressedDirt();
         tripleCompressedDirt = new BlockTripleCompressedDirt();
+        quadrupleCompressedDirt = new BlockQuadrupleCompressedDirt();
+        quintupleCompressedDirt = new BlockQuintupleCompressedDirt();
+        sextupleCompressedDirt = new BlockSextupleCompressedDirt();
+        septupleCompressedDirt = new BlockSeptupleCompressedDirt();
+        octupleCompressedDirt = new BlockOctupleCompressedDirt();
     }
 
-    public static void initModels()
-    {
+    public static void initModels() {
         compressedDirt.initModel();
         doubleCompressedDirt.initModel();
         tripleCompressedDirt.initModel();
+        quadrupleCompressedDirt.initModel();
+        quintupleCompressedDirt.initModel();
+        sextupleCompressedDirt.initModel();
+        septupleCompressedDirt.initModel();
+        octupleCompressedDirt.initModel();
     }
 
 }
